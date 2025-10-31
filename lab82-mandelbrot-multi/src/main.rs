1232c850c41bba91ab1cc3e06b2b21538d95f7ab
Pana Wanitchollakit <pnaw.wan@gmail.com>
2025-10-31 11:02:19 +0700

    done lab82

--- lab82-mandelbrot-multi/src/main.rs
+++ lab82-mandelbrot-multi/src/main.rs
@@ -19,18 +19,34 @@ fn main() {
     let start = Instant::now();
 
     // TODO: Calculate all pixels in parallel (based on lab 81-mandelbrot-single)
+    let pixels: Vec<(u32, u32, Rgb<u8>)> = (0..image_height)
+    .into_par_iter()
+    .flat_map(|y| {
+        (0..image_width).into_par_iter().map(move |x| {
+            let cx = x_min + (x as f64 / image_width as f64) * (x_max - x_min);
+            let cy = y_min + (y as f64 / image_height as f64) * (y_max - y_min);
+            let c = Complex::new(cx, cy);
 
+            let mut z = Complex::new(0.0, 0.0);
+            let mut iteration = 0;
 
-    // Placeholder for pixel calculations
-    let pixels: Vec<(u32, u32, Rgb<u8>)> =
-        (0..image_height)
-        .flat_map(|y| {
-            (0..image_width).map(move |x| {
-                let pixel: Rgb<u8> = Rgb([0, 0, 0]);
-                (x, y, pixel)
-            })
+            while iteration < max_iterations && z.norm_sqr() <= 4.0 {
+                z = z * z + c;
+                iteration += 1;
+            }
+
+            let hue = (iteration as f32 / max_iterations as f32) * 360.0;
+            let pixel = if iteration < max_iterations {
+                hsv_to_rgb(hue, 1.0, 1.0)
+            } else {
+                Rgb([0, 0, 0])
+            };
+
+            (x, y, pixel)
         })
-        .collect();
+    })
+    .collect();
+
     // Write pixels to image buffer
     for (x, y, pixel) in pixels {
         imgbuf.put_pixel(x, y, pixel);
